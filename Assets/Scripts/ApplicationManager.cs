e994e546c6b08eb13c6325cbcbb54f15cb41630e
Michael Noseworthy <michael.noseworthy@georgebrown.ca>
2018-12-03 20:18:53 -0500

    Added Game Menu's

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationManager : MonoBehaviour {
+
+    public void LoadFightTest()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("FightTest");
+    }
+
+    public void LoadMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu 3D");
+    }
+
+    public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
