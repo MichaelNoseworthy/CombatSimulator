901dab53b19d22bf08891471b761b9cd2b8fe927
MikeTheNose <michael.noseworthy@georgebrown.ca>
2018-12-04 15:12:23 -0500

    Minor updates to Main Menu and bug fixes
    Fixed bug that I created when making the Main Menu that caused the death animation to not appear.
    
    Added the ability to use specific animations for all characters upon death on the script.

--- Assets/Scripts/Characters/OnDeath.cs
+++ Assets/Scripts/Characters/OnDeath.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class OnDeath : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    public bool Melee = false;//character type
+    [SerializeField]
+    public bool Ranged = false;//character type
+    [SerializeField]
+    public bool RockThrower = false;//character type
+    [SerializeField]
+    public bool MagicUser = false;//character type
+
+    Animator m_Animator;//Gets the animations for this AI
+
+    // Use this for initialization
+    void Start () {
+        m_Animator = GetComponent<Animator>();
         Destroy(gameObject, 5);
-	}
+
+        if (MagicUser)
+        {
+            setAnimation("death");
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void setAnimation(string anim)
+    {
+        m_Animator.Play(anim);
+    }
 }
